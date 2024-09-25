61fdd24788bbff40dd16f122b6edb3f9e9c760a9
Nelson436 <nelsonzumaya07@gmail.com>
2024-09-25 07:09:49 -0700

    Add files via upload

---
+++ carito.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minipoo
+{
+    internal class carito
+    {
+        public List<producto>Lista {  get; set; }=new List<producto>();
+        public void Mostrarcarito()
+
+        Lista.Add(producto);
+
+        public void AgregarProducto(producto producto)
+    }
+}
