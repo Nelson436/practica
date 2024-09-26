2dee9e7d1e73aced060cab59bb58d50ef6911db0
Nelson436 <nelsonzumaya07@gmail.com>
2024-09-26 10:08:13 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace minipoo
+namespace Tienda_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-        string nombre = "";
-        int cantidad = 0;
-        decimal precio = 0;
-        Console.WriteLine("Ingresa nombre del producto");
-        nombre = Console.ReadLine();
-        Console.WriteLine("ingresa cantidad");
-        cantidad=Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("ingresa precio");
-        precio=Convert.ToDecimal(Console.ReadLine());
+            Carito carrito = new Carito();
 
+            while (true)
+            {
+                Console.WriteLine("Selecciona el articulo (ingresa '0' para finalizar):");
+                Catalogo.MostrarCatalogo();
+
+                int artID = Convert.ToInt32(Console.ReadLine());
+
+                if (artID == 0)
+                {
+                    break;
+                }
+
+                Articulo articuloSeleccionado = Catalogo.BuscarArticuloPorID(artID);
+
+                if (articuloSeleccionado != null)
+                {
+                    carrito.AgregarArticulo(articuloSeleccionado);
+                    Console.WriteLine($"Articulo '{articuloSeleccionado.Nombre}' agregado al carrito.");
+                }
+                else
+                {
+
+                }
+            }
         }
     }
 }
